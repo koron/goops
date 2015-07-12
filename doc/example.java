da4950c3a9f898d489f54aae858d07ec828c225d
MURAOKA Taro <koron.kaoriya@gmail.com>
2015-07-13 00:44:03 +0900

    doc

---
+++ doc/example.java
@@ -0,0 +1,13 @@
+package net.kaoriya.goops;
+
+public class GoopsExample {
+
+    public static int sumInt(int n) {
+        sum := 0
+        for i := 1; i < n; i++ {
+            sum += i
+        }
+        return sum
+    }
+
+}
